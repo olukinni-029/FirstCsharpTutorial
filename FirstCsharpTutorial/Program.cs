2717a769b43edeee1f108c3aa31d7c3a2152764e
olukinni_029 <olukinnif`150216029@gmail.com>
2023-07-03 11:18:31 +0100

    Add project files.

---
+++ FirstCsharpTutorial/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstCsharpTutorial
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            Console.WriteLine("Press any key to exit...");
+            Console.Read();
+           
+        }
+
+    }
+}
